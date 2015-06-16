7e42cd627005f07cc1ea990899492ee19e371d01
andy_morpheus <snooppy06@mail.ru>
2015-06-16 04:43:44 +0300

    1.0.1

--- script.py
+++ script.py
@@ -15,8 +15,9 @@ def tell_me_somth():
 def scan_me_all():
 	while True:
 		now=datetime.now()
-		now_time='(%s:%s:%s)' % (now.hour, now.minute, now.second)
-		now_date='(%s:%s:%s)' % (now.day, now.month, now.year)
+#		now_time='%s:%s:%s' % (now.hour, now.minute, now.second)
+#		now_date='%s:%s:%s' % (now.day, now.month, now.year)
+		now_date_time='%s-0%s-%s %s:%s:%s' % (now.year, now.month, now.day, now.hour, now.minute, now.second)
 		p=subprocess.Popen("ps -aux | awk '{sum[$1] += $3}END {for(i in sum)print i \":\"sum[i]}'", shell=True, stdout=subprocess.PIPE)
 		a=[]
 		db = MySQLdb.connect(host="localhost", user="sprint", passwd="sprint", db="spirit", charset='utf8')
@@ -26,7 +27,7 @@ def scan_me_all():
 			if not strin_answ: break
 			a.append(strin_answ.split(":"))
 		for elem in a:
-        		sql = """INSERT INTO cpu(time, date, user, cpu) VALUES ('%(time)s', '%(date)s', '%(user)s', '%(cpu)s')"""%{"time":(str(now_time)), "date":(str(now_date)), "user":str(elem[0]), "cpu":str(elem[1])}
+        		sql = """INSERT INTO cpu(date_time, user, cpu) VALUES ('%(date_time)s', '%(user)s', '%(cpu)s')"""%{"time":(str(now_date_time)), "user":str(elem[0]), "cpu":str(elem[1])}
 			cursor.execute(sql)
 		db.commit()
 		db.close()
