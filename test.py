ccca4a6942d31b9ea9df675582857c34762c6a18
andy_morpheus <snooppy06@mail.ru>
2015-06-16 04:53:20 +0300

    1.0.2

--- test.py
+++ test.py
@@ -7,11 +7,11 @@ import MySQLdb
 from datetime import datetime
 
 def scan_me_all():
-    while True:
+#    while True:
         now=datetime.now()
 #        now_time='%s:%s:%s' % (now.hour, now.minute, now.second)
 #        now_date='%s:%s:%s' % (now.day, now.month, now.year)
-        now_date_time='%s-0%s-%s %s:%s:%s' % (now.year, now.month, now.day, now.hour, now.minute, now.second)
+        now_date_time='%s-%s-%s %s:%s:%s' % (now.year, now.month, now.day, now.hour, now.minute, now.second)
         print now_date_time
         p=subprocess.Popen("ps -aux | awk '{sum[$1] += $3}END {for(i in sum)print i \":\"sum[i]}'", shell=True, stdout=subprocess.PIPE)
         a=[]
@@ -22,10 +22,10 @@ def scan_me_all():
             if not strin_answ: break
             a.append(strin_answ.split(":"))
         for elem in a:
-                sql = """INSERT INTO cpu(date_time, user, cpu) VALUES ('%(date_time)s', '%(user)s', '%(cpu)s')"""%{"time":(str(now_date_time)), "user":str(elem[0]), "cpu":str(elem[1])}
-            cursor.execute(sql)
+        	sql = """INSERT INTO cpu(date_time, user, cpu) VALUES ('%(date_time)s', '%(user)s', '%(cpu)s')"""%{"date_time":(str(now_date_time)), "user":str(elem[0]), "cpu":str(elem[1])}
+        	cursor.execute(sql)
         db.commit()
         db.close()
-        time.sleep(300)
+#        time.sleep(300)
         
 scan_me_all()
