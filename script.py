412dc4713fdcfbd4139627aa45d7a1dc813936c8
Andy <andy.prih@gmail.com>
2015-06-19 01:41:13 +0300

    dsad

--- script.py
+++ script.py
@@ -13,25 +13,26 @@ def tell_me_somth():
 	pass
 
 def scan_me_all():
-	while True:
-		now=datetime.now()
-#		now_time='%s:%s:%s' % (now.hour, now.minute, now.second)
-#		now_date='%s:%s:%s' % (now.day, now.month, now.year)
-		now_date_time='%s-0%s-%s %s:%s:%s' % (now.year, now.month, now.day, now.hour, now.minute, now.second)
-		p=subprocess.Popen("ps -aux | awk '{sum[$1] += $3}END {for(i in sum)print i \":\"sum[i]}'", shell=True, stdout=subprocess.PIPE)
-		a=[]
-		db = MySQLdb.connect(host="localhost", user="sprint", passwd="sprint", db="spirit", charset='utf8')
-		cursor = db.cursor()
-		while True:
-			strin_answ=p.stdout.readline()
-			if not strin_answ: break
-			a.append(strin_answ.split(":"))
-		for elem in a:
-        		sql = """INSERT INTO cpu(date_time, user, cpu) VALUES ('%(date_time)s', '%(user)s', '%(cpu)s')"""%{"time":(str(now_date_time)), "user":str(elem[0]), "cpu":str(elem[1])}
-			cursor.execute(sql)
-		db.commit()
-		db.close()
-		time.sleep(300)
+    while True:
+        now=datetime.now()
+		#now_time='%s:%s:%s' % (now.hour, now.minute, now.second)
+		#now_date='%s:%s:%s' % (now.day, now.month, now.year)
+        now_date_time='%s-%s-%s %s:%s:%s' % (now.year, now.month, now.day, now.hour, now.minute, now.second)
+        #print now_date_time
+        p=subprocess.Popen("ps -aux | awk '{sum[$1] += $3}END {for(i in sum)print i \":\"sum[i]}'", shell=True, stdout=subprocess.PIPE)
+        a=[]
+        db = MySQLdb.connect(host="localhost", user="sprint", passwd="sprint", db="spirit", charset='utf8')
+        cursor = db.cursor()
+        while True:
+            strin_answ=p.stdout.readline()
+            if not strin_answ: break
+            a.append(strin_answ.split(":"))
+        for elem in a:
+        	sql = """INSERT INTO cpu(date_time, user, cpu) VALUES ('%(date_time)s', '%(user)s', '%(cpu)s')"""%{"date_time":(str(now_date_time)), "user":str(elem[0]), "cpu":str(elem[1])}
+        	cursor.execute(sql)
+        db.commit()
+        db.close()
+#        time.sleep(300)
 
 
 def wait_take_it_easy():
